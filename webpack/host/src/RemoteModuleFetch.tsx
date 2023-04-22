5fc41047c080c74a5bc71a4bd8003159393b7fec
Hevon Gordon <hevongordon.professional@gmail.com>
2023-04-22 00:19:01 -0500

    updated remote fetch component to use remote module

--- webpack/host/src/RemoteModuleFetch.tsx
+++ webpack/host/src/RemoteModuleFetch.tsx
@@ -11,13 +11,44 @@ interface RemoteModuleProps<T> {
     [key: string | number]: T;
 }
 
+interface IWindow extends Window {
+    [key: string]: any;
+  }
+
+declare const __webpack_init_sharing__: (shareScope: string) => Promise<void>;
+declare const __webpack_share_scopes__: { default: string };
+
+function loadComponent<T>(scope: string, module: string): () => Promise<T> {
+  return async () => {
+    // Initializes the share scope. This fills it with known provided modules from this build and all remotes
+    await __webpack_init_sharing__('default');
+    const _window = window as IWindow;
+    const container = _window[scope]; // or get the container somewhere else
+    // Initialize the container, it may provide shared modules
+    await container.init(__webpack_share_scopes__.default);
+    const factory = await _window[scope].get(module);
+    const Module = factory() as T;
+    return Module;
+  };
+}
+
+const useDynamicScript = (url: string) => {
+      const element = document.createElement("script");
+      element.src = url;
+      element.type = "text/javascript";
+      element.async = true;
+      document.head.appendChild(element);
+  };
+
 // This is a generic component that will fetch a remote module and render it
 const RemoteModuleFetch = function <T>(props: RemoteModuleFetchProps<T>) {
     const [RemoteModule, setRemoteModule] = useState<FC<RemoteModuleProps<T>> | null>(null);
     const { remoteModuleProps, remoteModuleLocation } = props;
+    
     useEffect(() => {
+        useDynamicScript(remoteModuleLocation);
         const fetchRemoteModule = async () => {
-            const remoteModule = React.lazy(() => import(`outsiders/${remoteModuleLocation}.js`)) as FC<RemoteModuleProps<T>>;
+            const remoteModule = React.lazy(loadComponent('app2', './Widget')) as FC<RemoteModuleProps<T>>;
             setRemoteModule(remoteModule);
         };
         fetchRemoteModule();
